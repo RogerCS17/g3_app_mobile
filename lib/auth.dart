8956d2bc0138e8d9d15ba9a088d65cd7d760f4f8
Sebastian Hidalgo <sebastian.hidalgo@unmsm.edu.pe>
2024-06-15 10:01:19 -0500

    refactor(global): change auth funcs. to new backend

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,63 +1,54 @@
-import 'package:flutter/widgets.dart';
-import 'package:g3_app_mobile/models/auth.model.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+import 'package:g3_app_mobile/models/auth.model.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
-// firebase api key
-const apiKey = "AIzaSyDi6K72pVbUc1FGDy1nZ0CeKgYqjDvVAbc";
+const apiURL = "http://10.0.2.2:8000";
+
+Map<String, String> buildHeaders() {
+  return {
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+}
 
 Future<String> postRegisterUser(
     String email, String password, BuildContext context) async {
-  Map<String, String> headers = {
-    'Content-Type': 'application/json; charset=UTF-8',
-  };
-  final body = jsonEncode(
-      {'email': email, 'password': password, 'returnSecureToken': true});
+  final headers = buildHeaders();
+  final body = jsonEncode({"email": email, "password": password});
 
-  var response = await http.post(
-    Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey'),
-    headers: headers,
-    body: body,
-  );
+  var res =
+      await http.post(Uri.parse("$apiURL/users"), headers: headers, body: body);
 
-  var data = jsonDecode(response.body);
+  var data = jsonDecode(res.body);
 
-  if (data["error"] != null) {
-    return data["error"]["message"];
+  if (data["detail"] != null) {
+    return data["detail"][0]["ctx"]["error"];
   }
 
   Provider.of<AuthModel>(context, listen: false).setAuthenticated(true);
-  Provider.of<AuthModel>(context, listen: false).setUid(data["idToken"]);
+  Provider.of<AuthModel>(context, listen: false).setUid("${data["id"]}");
   Provider.of<AuthModel>(context, listen: false).setEmail(data["email"]);
   return "";
 }
 
 Future<String> postLoginUser(
     String email, String password, BuildContext context) async {
-  Map<String, String> headers = {
-    'Content-Type': 'application/json; charset=UTF-8',
-  };
-  final body = jsonEncode(
-      {'email': email, 'password': password, 'returnSecureToken': true});
+  final headers = buildHeaders();
+  final body = jsonEncode({"email": email, "password": password});
 
-  var response = await http.post(
-    Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$apiKey'),
-    headers: headers,
-    body: body,
-  );
+  var res =
+      await http.post(Uri.parse("$apiURL/auth"), headers: headers, body: body);
 
-  var data = jsonDecode(response.body);
+  var data = jsonDecode(res.body);
 
   if (data["error"] != null) {
-    return data["error"]["message"];
+    return data["error"];
   }
 
   Provider.of<AuthModel>(context, listen: false).setAuthenticated(true);
-  Provider.of<AuthModel>(context, listen: false).setUid(data["idToken"]);
+  Provider.of<AuthModel>(context, listen: false).setUid("${data["id"]}");
   Provider.of<AuthModel>(context, listen: false).setEmail(data["email"]);
   return "";
 }
